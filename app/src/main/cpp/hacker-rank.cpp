dd7d21c74dd2407a61744c9fd2a5d0ca31699920
John Masiello <jmasgcc@gmail.com>
2018-08-01 18:56:21 -0700

    Set up a workspace to take a test on hackerrank.com

---
+++ app/src/main/cpp/hacker-rank.cpp
@@ -0,0 +1,87 @@
+//
+// Created by john on 8/1/18.
+//
+#include "hacker-rank.h"
+#include "util.h"
+#include <string>
+
+using namespace std;
+
+namespace hacker_one {
+    const int MY_CONST = 11;
+
+    int main() {
+        return MY_CONST;
+    }
+
+
+
+
+
+
+
+}
+
+namespace hacker_two {
+    const int MY_CONST = 22;
+
+    int main() {
+        return MY_CONST;
+    }
+
+
+
+
+
+
+}
+
+namespace hacker_three {
+    int main() {
+        return 3;
+    }
+
+
+
+
+
+
+
+
+}
+
+namespace hacker_four {
+    int main() {
+        return 4;
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+/* The main driver
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+int main() {
+//    COUT(to_string(hacker_one::main()).data());
+//    COUT(to_string(hacker_two::main()).data());
+
+
+    return 0;
+}
